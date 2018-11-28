458e5feb52aef71e54968d0a04965361888a82de
Joseph Parker <jparker010190@gmail.com>
2018-11-28 00:35:30 +0000

    automatically backed up by learn

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,11 @@
 // add Comment component here
+import React, {Component} from 'react'
+class BlogContent extends React.Component{
+  render(){
+    return(
+      <div class="comment">
+      {this.props.commentText}
+      </div>
+    )
+  }
+}
